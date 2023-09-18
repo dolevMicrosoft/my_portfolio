c41bd0ecc6673f33a8094003aac657b96262f062
Dolev brami <dolevbrami2005@gmail.com>
2023-09-18 19:12:07 +0300

    finished home ssection and started about me section

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-
-
 const navLinks = document.querySelectorAll(".nav-link");
-
+const sections = document.querySelectorAll("section");
 console.log(navLinks);
-navLinks.forEach((link)=>{link.addEventListener('click', (event)=>{
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
-    navLinks.forEach(function(link) {
-        link.classList.remove('active');
-      });
 
-    link.classList.add('active');
-    console.log("Hello world");
+    navLinks.forEach(function (link) {
+      link.classList.remove("active");
+    });
+    link_name = link.textContent.toString()
+    if(link_name == "About me"){
+      scrollToSection("About-me");
+    }else{
+      scrollToSection(link_name);
+    }
+    
+    link.classList.add("active");
+  });
 });
+sections.forEach((section) => {
+  section.addEventListener();
 });
+
+function scrollToSection(sectionId) {
+  console.log(sectionId)
+  const section = document.getElementById(sectionId);
+  if (section) {
+    console.log("+1")
+    section.scrollIntoView({ behavior: "smooth" }); // Smooth scrolling
+  }
+  else{
+    console.log("-1")
+  }
+}
